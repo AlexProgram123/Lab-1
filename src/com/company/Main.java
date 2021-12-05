7678d75ac3415b22c3a599337d37494be1666ea2
AlexProgram123 <avd47@tpu.ru>
2021-12-05 18:03:41 +0300

    i

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,16 @@ public class Main {
         Forniture[] allForniture = new Forniture[N];
 
         for(int i=0; i<allForniture.length; i++) {
-            allForniture[i] = new Chair(Math.ceil(Math.random()*100));
-            allForniture[i].setName("Стул №"+(i));
-            allForniture[i].setPrice(Math.ceil(Math.random() * 1000));
-
+            if(Math.random()<0.5) {
+                allForniture[i] = new Chair(Math.ceil(Math.random() * 100));
+                allForniture[i].setName("Стул №" + (i));
+                allForniture[i].setPrice(Math.ceil(Math.random() * 1000));
+            }
+            else {
+                allForniture[i] = new Cooker(Math.ceil(Math.random() * 10000));
+                allForniture[i].setName("Плита №" + (i));
+                allForniture[i].setPrice(Math.ceil(Math.random() * 10000));
+            }
         }
 
         for(int i=1; i<allForniture.length; i++){
